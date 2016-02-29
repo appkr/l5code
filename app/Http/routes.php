91c2a8b00830d729534013a7cec39efefbbd47b6
appkr <juwonkim@me.com>
2016-03-01 01:03:56 +0900

    뷰와 데이터 바인딩

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,21 @@
 <?php
 
 Route::get('/', function () {
-    return view('welcome');
-    //return '<h1>Hello Foo</h1>';
+    return view('errors.503');
+
+    //return view('errors/503');
+
+    //return View::make('errors/503');
+
+    //return view('welcome')->with('name', 'Foo');
+
+    //return view('welcome')->with([
+    //    'name' => 'Foo',
+    //    'greeting' => '안녕하세요?',
+    //]);
+
+    //return view('welcome', [
+    //    'name' => 'Foo',
+    //    'greeting' => '안녕하세요?',
+    //]);
 });
