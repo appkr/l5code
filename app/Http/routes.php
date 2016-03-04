8e6653085dc180310a8fe46d61c6547aaa3240a6
appkr <juwonkim@me.com>
2016-03-05 01:57:06 +0900

    사용자 인증

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,4 +2,37 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::resource('articles', 'ArticlesController');
+Route::get('auth/login', function () {
+    $credentials = [
+        'email' => 'john@example.com',
+        'password' => 'password',
+    ];
+
+    if (! auth()->attempt($credentials)) {
+        return '로그인 정보가 정확하지 않습니다.';
+    }
+
+    return redirect('protected');
+});
+
+Route::get('protected', ['middleware' => 'auth', function () {
+    var_dump(session()->all());
+
+//    if (! auth()->check()) {
+//        return '누구세요?';
+//    }
+
+    return '어서오세요 ' . auth()->user()->name;
+}]);
+
+Route::get('auth/logout', function () {
+    auth()->logout();
+
+    return '또 봐요~';
+});
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('password/remind', 'Auth\PasswordController@getEmail');
