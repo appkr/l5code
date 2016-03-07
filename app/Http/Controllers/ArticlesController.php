82c24295b603cdcc9a1a50e45ab4adfb23d126c6
appkr <juwonkim@me.com>
2016-03-08 00:58:06 +0900

    즉시 로드와 페이징

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -13,7 +13,14 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        return __METHOD__ . '은(는) Article 컬렉션을 조회합니다.';
+//        $articles = \App\Article::with('user')->get();
+
+//        $articles = \App\Article::get();
+//        $articles->load('user');
+
+        $articles = \App\Article::latest()->paginate(3);
+
+        return view('articles.index', compact('articles'));
     }
 
     /**
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,37 +2,8 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('auth/login', function () {
-    $credentials = [
-        'email' => 'john@example.com',
-        'password' => 'password',
-    ];
+Route::resource('articles', 'ArticlesController');
 
-    if (! auth()->attempt($credentials)) {
-        return '로그인 정보가 정확하지 않습니다.';
-    }
-
-    return redirect('protected');
-});
-
-Route::get('protected', ['middleware' => 'auth', function () {
-    var_dump(session()->all());
-
-//    if (! auth()->check()) {
-//        return '누구세요?';
-//    }
-
-    return '어서오세요 ' . auth()->user()->name;
-}]);
-
-Route::get('auth/logout', function () {
-    auth()->logout();
-
-    return '또 봐요~';
-});
-
-Route::auth();
-
-Route::get('/home', 'HomeController@index');
-
-Route::get('password/remind', 'Auth\PasswordController@getEmail');
+//DB::listen(function($query){
+//    var_dump($query->sql);
+//});
