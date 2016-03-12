4e777df13dde6e7c159b4d1ff9f5699b258debef
appkr <juwonkim@me.com>
2016-03-12 17:14:04 +0900

    엘릭서와 프런트 엔드

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,21 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+  mix.sass('app.scss');
+
+  mix.scripts([
+    '../../../node_modules/jquery/dist/jquery.js',
+    '../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
+    '../../../node_modules/highlightjs/highlight.pack.js',
+    'app.js'
+  ], 'public/js/app.js');
+
+  mix.version([
+    'css/app.css',
+    'js/app.js'
+  ]);
+
+  //mix.copy('node_modules/font-awesome/fonts', 'public/build/fonts');
+
+  mix.browserSync({proxy: 'localhost:8000'});
 });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,12 +8,12 @@
     <title>Laravel</title>
 
     <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    {{--<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>--}}
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
     <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    {{--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">--}}
+     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 
     <style>
         body {
@@ -81,8 +81,8 @@
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>--}}
+    {{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>--}}
+     <script src="{{ elixir('js/app.js') }}"></script>
 </body>
 </html>
