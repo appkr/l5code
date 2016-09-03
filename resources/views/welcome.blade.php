0411319be61626b2c5f31f568020dc10046c69bc
appkr <juwonkim@me.com>
2016-09-03 18:39:54 +0900

    뷰와 데이터 바인딩

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,4 @@
-<h1>Hello Laravel</h1>
+<h1>
+    <?= isset($greeting) ? "{$greeting} " : 'Hello '; ?>
+    <?= $name; ?>
+</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,23 @@
 <?php
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('errors.503');
+});
 
-//Route::get('/', function () {
-//    return '<h1>Hello Foo</h1>';
-//});
+Route::get('/', function () {
+    return view('welcome')->with('name', 'Foo');
+});
 
-//Route::get('/{foo}', function ($foo) {
-//    return $foo;
-//});
+Route::get('/', function () {
+    return view('welcome')->with([
+        'name' => 'Foo',
+        'greeting' => '안녕하세요?',
+    ]);
+});
 
-//Route::get('/{foo?}', function ($foo = 'bar') {
-//    return $foo;
-//});
-
-//Route::pattern('foo', '[0-9a-zA-Z]{3}');
-//Route::get('/{foo?}', function ($foo = 'bar') {
-//    return $foo;
-//});
-
-//Route::get('/{foo?}', function ($foo = 'bar') {
-//    return $foo;
-//})->where('foo', '[0-9a-zA-Z]{3}');
-
-Route::get('/', [
-    'as' => 'home',
-    function () {
-        return '제 이름은 "home" 입니다.';
-    }
-]);
-
-Route::get('/home', function () {
-    return redirect(route('home'));
+Route::get('/', function () {
+    return view('welcome', [
+        'name' => 'Foo',
+        'greeting' => '환영합니다!',
+    ]);
 });
