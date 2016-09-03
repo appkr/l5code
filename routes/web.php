63319f6492e0a8096f1e51e7bdae142229c806b2
appkr <juwonkim@me.com>
2016-09-03 18:15:49 +0900

    라우팅

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,37 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return '<h1>Hello Foo</h1>';
+//});
+
+//Route::get('/{foo}', function ($foo) {
+//    return $foo;
+//});
+
+//Route::get('/{foo?}', function ($foo = 'bar') {
+//    return $foo;
+//});
+
+//Route::pattern('foo', '[0-9a-zA-Z]{3}');
+//Route::get('/{foo?}', function ($foo = 'bar') {
+//    return $foo;
+//});
+
+//Route::get('/{foo?}', function ($foo = 'bar') {
+//    return $foo;
+//})->where('foo', '[0-9a-zA-Z]{3}');
+
+Route::get('/', [
+    'as' => 'home',
+    function () {
+        return '제 이름은 "home" 입니다.';
+    }
+]);
+
+Route::get('/home', function () {
+    return redirect(route('home'));
 });
