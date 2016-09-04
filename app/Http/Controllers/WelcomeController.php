3116910321037e3701ebbac6a14729f8e2abb10e
appkr <juwonkim@me.com>
2016-09-04 16:51:26 +0900

    컨트롤러

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class WelcomeController extends Controller
+{
+    public function index() {
+        return view('welcome');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,5 @@
 <?php
 
-Route::get('/', function () {
-    return view('errors.503');
-});
+Route::get('/', 'WelcomeController@index');
 
-Route::get('/', function () {
-    return view('welcome')->with('name', 'Foo');
-});
-
-Route::get('/', function () {
-    return view('welcome')->with([
-        'name' => 'Foo',
-        'greeting' => '안녕하세요?',
-    ]);
-});
-
-Route::get('/', function () {
-    return view('welcome', [
-        'name' => 'Foo',
-        'greeting' => '환영합니다!',
-    ]);
-});
-
-Route::get('/', function () {
-    $items = ['apple', 'banana', 'tomato'];
-
-    return view('welcome', ['items' => $items]);
-});
-
-// 페이지 30
-// 템플릿 상속을 테스트하기 위한 라우트
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('articles', 'ArticlesController');
