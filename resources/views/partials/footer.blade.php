4d0d221be883f49240cbfadd3516f4aa1bd8da0d
appkr <juwonkim@me.com>
2016-09-04 14:50:24 +0900

    블레이드

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<footer>
+  <p>저는 꼬리말입니다. 다른 뷰에서 저를 입양해 가요.</p>
+</footer
+
+@section('script')
+  <!--코드 5-15-->
+  @parent
+  <script>
+    // 코드 5-14
+    alert("저는 조각 뷰의 'script' 섹션입니다.");
+  </script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,15 @@ Route::get('/', function () {
         'greeting' => '환영합니다!',
     ]);
 });
+
+Route::get('/', function () {
+    $items = ['apple', 'banana', 'tomato'];
+
+    return view('welcome', ['items' => $items]);
+});
+
+// 페이지 30
+// 템플릿 상속을 테스트하기 위한 라우트
+Route::get('/', function () {
+    return view('welcome');
+});
