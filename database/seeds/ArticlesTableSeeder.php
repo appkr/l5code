0e70e4474b1583f5db39a420e5c1529c696a804f
appkr <juwonkim@me.com>
2016-09-10 11:32:19 +0900

    데이터베이스 시딩

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = App\User::all();
+
+        $users->each(function ($user) {
+            $user->articles()->save(
+                factory(App\Article::class)->make()
+            );
+        });
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        if (config('database.default') !== 'sqlite') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        }
+
+//        Model::unguard();
+
+        App\User::truncate();
+        $this->call(UsersTableSeeder::class);
+
+        App\Article::truncate();
+        $this->call(ArticlesTableSeeder::class);
+
+//        Model::reguard();
+
+        if (config('database.default') !== 'sqlite') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        }
     }
 }
