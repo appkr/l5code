57729b5a3f0b67e8c945c92123751a3e6ec5cfe7
appkr <juwonkim@me.com>
2017-03-07 14:04:11 +0900

    첨부파일과 아티클을 비동기로 생성할 때, 아티클과 첨부파일이 associate 되지 않는 문제 수정

--- app/Http/Requests/ArticlesRequest.php
+++ app/Http/Requests/ArticlesRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use App\Attachment;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ArticlesRequest extends FormRequest
@@ -39,7 +41,34 @@ class ArticlesRequest extends FormRequest
             'tags' => ['required', 'array'],
             'content' => ['required', 'min:10'],
             'files' => ['array'],
-            'files.*' => ['sometimes', "mimes:{$mimes}", 'max:30000'],
+            'files.*' => ["mimes:{$mimes}", 'max:30000'],
+            'attachments' => ['array'],
+            'attachments.*' => ['integer', 'exists:attachments,id'],
         ];
+    }
+
+    /**
+     * 'notification' 입력 값을 머지한 사용자 입력값을 조회합니다.
+     *
+     * @return array
+     */
+    public function getPayload()
+    {
+        return array_merge($this->all(), [
+            'notification' => $this->has('notification'),
+        ]);
+    }
+
+    /**
+     * 사용자 입력 값으로부터 첨부파일 객체를 조회합니다.
+     *
+     * @return Collection
+     */
+    public function getAttachments()
+    {
+        return Attachment::whereIn(
+            'id',
+            $this->input('attachments', [])
+        )->get();
     }
 }
