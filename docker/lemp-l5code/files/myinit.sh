91eec9b43b9349d396a7945c06dab31e4f61a524
appkr <juwonkim@me.com>
2019-05-25 11:45:20 +0900

    Bind 80 and 3306, instead of 8000 and 33060

--- docker/lemp-l5code/files/myinit.sh
+++ docker/lemp-l5code/files/myinit.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 if [ ! -d $MYSQL_DATA_DIR/myapp ]; then
-    mysql -v -e "USE mysql; UPDATE mysql.user SET authentication_string = PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User = 'root' AND Host = '%';"
+    # homestead 사용자 생성
+    mysql -v -e "CREATE USER 'homestead'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
     mysql -v -e "CREATE USER 'homestead'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+    # homestead 사용자 권한 부여
+    mysql -v -e "GRANT ALL PRIVILEGES ON myapp.* TO 'homestead'@'localhost';"
     mysql -v -e "GRANT ALL PRIVILEGES ON myapp.* TO 'homestead'@'%';"
-    mysql -v -e "CREATE DATABASE myapp; FLUSH PRIVILEGES;"
+    mysql -v -e "FLUSH PRIVILEGES;"
+
+    # 데이터베이스 생성
+    mysql -v -e "CREATE DATABASE myapp;"
+
+    # 폴더에 쓰기 권한 부여
     chmod -R 775 storage /var/www/myapp/bootstrap/cache /var/www/myapp/public/files
+
+    # 테이블 마이그레이션 및 시딩
     php /var/www/myapp/artisan migrate --seed --force
 fi
